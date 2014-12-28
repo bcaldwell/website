c284b2b6c910f71117f34247a4936bee70a3dc8d
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2014-12-28 18:19:58 -0500

    added javascript back

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,28 @@
+$(window).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 150) {
+      $("header").addClass("fix-header");
+    } else {
+      $("header").removeClass("fix-header");
+    }
+  });
+
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top-60
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+});
+
+<!-- http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links/ -->
+
+
