7e27923b68519511ae1cd3378d90d25765806889
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2014-12-31 13:21:43 -0500

    started projects section

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,32 +1,26 @@
-$('titlePicture').height(screen.height);
 $(window).ready(function () {
-  console.log(screen.height);
-  $('titlePicture').height(screen.height);
-
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 550) {
-      $("header").addClass("fix-header");
-    } else {
-      $("header").removeClass("fix-header");
-    }
-  });
-
-  $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top //-50
-          }, 1000);
-          return false;
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 550) {
+            $("header").addClass("fix-header");
+        } else {
+            $("header").removeClass("fix-header");
         }
-      }
     });
-  });
+
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top //-50
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 });
 
-<!-- http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links/ -->
-
-
+// http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links/
