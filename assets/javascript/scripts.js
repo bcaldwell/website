c2b60efec619f800dc18985d7cb0daa80cba2282
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2015-07-24 21:11:37 -0400

    new tracking

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -15,6 +15,22 @@ $(window).ready(function () {
   ];
   $("#backgroundImage").attr('src',images[Math.floor(Math.random()*12)]);
 
+  <!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.benjamincaldwell.me/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//analytics.benjamincaldwell.me/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -56,7 +72,7 @@ $(window).ready(function () {
       $('a[href$="contact-me"]').css("background-color","rgba(0, 0, 0, 0.3)");
     }
   });
-  
+
   $(".dropdownButton").hover(
     function () {
       $('.dropdown').slideDown('medium', function () {
